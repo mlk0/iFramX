afbe0212e14a220b377b10e30e7beed61c7cf534
m1k0 <mirko.atanasov@gmail.com>
2017-01-12 07:22:58 -0500

    'comment'

---
+++ iFrameDemo/Controllers/FramePaymentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace iFrameDemo.Controllers
+{
+    public class FramePaymentController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            //return View();
+         
+            return View("Index", "~/Views/Shared/_LayoutLean.cshtml"); 
+        }
+
+
+        [HttpPost]
+        public string Pay(string someArgument = "default123") {
+            return someArgument;
+        }
+
+        [HttpPost]
+        public string Pay1(string id = "defaultID")
+        {
+            return id;
+        }
+    }
+}
